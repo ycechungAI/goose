673e4a25198c419cfe96cfd1152eaf823925ee37
Rizel Scarlett <rizel@squareup.com>
2025-07-24 12:49:56 -0400

    Fixing grants path (#3632)

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -451,7 +451,7 @@ const config: Config = {
     announcementBar: {
       id: 'goose-grants',
       content:
-        '✨ goose grant program now open: <a href="grants">apply now</a>! ✨',
+        '✨ goose grant program now open: <a href="/goose/grants">apply now</a>! ✨',
       backgroundColor: '#20232a',
       textColor: '#fff',
       isCloseable: false,
